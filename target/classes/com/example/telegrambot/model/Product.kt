9fcf70b609a6141eed35ff5e6e5e9c6cddd1c86b
Katherine Benjumea <kathe07338@gmail.com>
2026-03-08 22:16:43 -0500

    gitignore

--- target/classes/com/example/telegrambot/model/Product.kt
+++
@@ -1,9 +0,0 @@
-package com.example.telegrambot.model
-
-data class Product(
-        val code: Int,
-        var name: String,
-        var price: Double,
-        var quantity: Int,
-        val initialQuantity: Int
-)
